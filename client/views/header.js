479620d0b711d49911cd648f82de23280762b224
Eugene Sysmanov <appigram@gmail.com>
2016-04-03 03:51:19 +0300

    SCSS variables added

--- client/views/header.js
+++ client/views/header.js
@@ -1,3 +1,36 @@
+/* On navigation template created */
+Template.header.onCreated(function() {
+  this.autorun(() => {
+    // Set subscriptions
+    this.subscribe('messages.all');
+  });
+});
+
+Template.header.helpers({
+  activeIfRouteNameIs: (routeName) => {
+    if (FlowRouter.getRouteName() === routeName) {
+      return 'active';
+    }
+    return '';
+  },
+  getUnreadCount: () => {
+    let unreadMessageCount = 0;
+    let messages = Messages.find({$or: [{ originatingFromId: Meteor.userId(), 'conversation.originatingFromDeleted': false }, {originatingToId: Meteor.userId(), 'conversation.originatingToDeleted': false}]}).forEach(function(msg) {
+      for (let x = 0; x < msg.conversation.length; x++) {
+        if (msg.conversation[x].to.userId === Meteor.userId() && !msg.conversation[x].to.read) {
+          unreadMessageCount++;
+        }
+      }
+    });
+
+    if (unreadMessageCount > 0) {
+      return ('(' + unreadMessageCount + ')');
+    } else {
+      return '';
+    }
+  }
+});
+
 Template.header.events({
   'click [data-id=sign-out]': function() {
     Meteor.logout(function(error) {
